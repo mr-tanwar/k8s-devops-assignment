6480b381d466a7615a1f3a45c90e45fb28cc40f3
mr-tanwar <mr.tanwar2020@gmail.com>
2026-06-25 21:38:00 +0530

    version updated

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -2,8 +2,11 @@ import { Request, Response, Router } from "express";
 
 const router = Router();
 
-router.get("/", (_req: Request, res: Response<{ status: string }>) => {
-  res.json({ status: "OK" });
-});
+router.get(
+  "/",
+  (_req: Request, res: Response<{ status: string; version: string }>) => {
+    res.json({ status: "OK", version: "v3" });
+  },
+);
 
 export default router;
